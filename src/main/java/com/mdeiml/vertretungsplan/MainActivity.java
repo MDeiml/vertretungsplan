ffa9e57d9d8e1eb4ad5bed74111fb40649ad0fe0
MDeiml <matthias@deiml.net>
2015-12-03 16:19:43 +0100

    Ein paar Sachen hinzugefügt

--- src/main/java/com/mdeiml/vertretungsplan/MainActivity.java
+++ src/main/java/com/mdeiml/vertretungsplan/MainActivity.java
@@ -3,13 +3,12 @@ package com.mdeiml.vertretungsplan;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+    
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
 }
