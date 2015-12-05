072fadde7e558c664a82830ed2f6c2e795119169
mdeiml <matthias@deiml.net>
2015-12-05 17:20:54 +0100

    Leere Tage filtern

--- src/main/java/com/mdeiml/vertretungsplan/UpdateVertretungsplan.java
+++ src/main/java/com/mdeiml/vertretungsplan/UpdateVertretungsplan.java
@@ -38,8 +38,23 @@ public class UpdateVertretungsplan extends AsyncTask<URL, String, Exception> {
                 src += line + '\n';
             }
 
+            int dayIndex = src.indexOf("<div>");
             int index = -1;
+            boolean emptyDay = true;
             while((index = src.indexOf("<tr class=\"normal", index+1)) >= 0) { // nächster Eintrag
+                int nextDayIndex = src.indexOf("<div>", dayIndex + 1);
+                if(nextDayIndex != -1 && index > nextDayIndex) {
+                    int beachtenI = src.indexOf("<table class=\"BitteBeachten", dayIndex);
+                    if(beachtenI != -1 && beachtenI < nextDayIndex)
+                        emptyDay = false;
+                    if(emptyDay) {
+                        src = src.substring(0, dayIndex) + src.substring(nextDayIndex);
+                        index = dayIndex;
+                    }else {
+                        dayIndex = nextDayIndex;
+                    }
+                    emptyDay = true;
+                }
                 int end = src.indexOf("</tr>", index); // Ende des Eintrags
                 int i = src.indexOf("<td class=\"VBlock", index); // 1. Spalte des Eintrags (Klasse)
                 if(i < 0 || i > end) // wenn keine Klasse in dem Eintrag ist überspringen
@@ -49,7 +64,17 @@ public class UpdateVertretungsplan extends AsyncTask<URL, String, Exception> {
                 String classes = src.substring(i+1, j).trim(); // Inhalt der 1. Spalte
                 if(!(classes.startsWith(klassenstufe) && classes.contains(klassenbuchstabe))) {
                     src = src.substring(0, index) + src.substring(end+5); // wenn nicht die eingestellte Klasse Zeile entfernen
+                }else {
+                    emptyDay = false;
                 }
+            }
+            int beachtenI = src.indexOf("<table class=\"BitteBeachten", dayIndex);
+            if(beachtenI != -1)
+                emptyDay = false;
+            if(emptyDay) {
+                int nextDayIndex = src.indexOf("</div>", dayIndex + 1)+6;
+                src = src.substring(0, dayIndex) + src.substring(nextDayIndex);
+                index = dayIndex;
             }
             publishProgress(src); // Vertretungsplan anzeigen
         }catch(Exception e) {
